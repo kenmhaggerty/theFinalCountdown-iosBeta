d4f1b9598118f4064062322624a07e981fe7214b
kenmhaggerty <kenmhaggerty@gmail.com>
2016-02-09 10:53:45 -0500

    Stubs out FISViewController with IBOutlets, IBActions, and constants.

--- theFinalCountdown/FISViewController.m
+++ theFinalCountdown/FISViewController.m
@@ -8,8 +8,21 @@
 
 #import "FISViewController.h"
 
-@interface FISViewController ()
+#define START_TEXT @"Start"
+#define START_COLOR [UIColor greenColor]
+#define CANCEL_TEXT @"Cancel"
+#define CANCEL_COLOR [UIColor redColor]
+#define PAUSE_TEXT @"Pause"
 
+@interface FISViewController ()
+@property (nonatomic, strong) IBOutlet UIView *pickerView;
+@property (nonatomic, strong) IBOutlet UIDatePicker *datePicker;
+@property (nonatomic, strong) IBOutlet UIView *timerView;
+@property (nonatomic, strong) IBOutlet UILabel *timerLabel;
+@property (nonatomic, strong) IBOutlet UIButton *leftButton;
+@property (nonatomic, strong) IBOutlet UIButton *rightButton;
+- (IBAction)actionLeftButton:(UIButton *)sender;
+- (IBAction)actionRightButton:(UIButton *)sender;
 @end
 
 @implementation FISViewController
@@ -17,24 +30,24 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
-
-
-
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    //
 }
-
-- (void)viewWillAppear:(BOOL)animated
-{
-
-
-}
-
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)actionLeftButton:(UIButton *)sender
+{
+    //
+}
+
+- (IBAction)actionRightButton:(UIButton *)sender
+{
+    //
 }
 
 @end
